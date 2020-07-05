a49c446d2f9b2dab0126bdb646f1305dd791b494
gabrieosl <ga.bri.el@msn.com>
2020-07-05 18:22:44 -0300

    feature: layout fixes + final navigation

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,10 +1,74 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import backArrow from '../../assets/back_arrow.png';
+import tableArrow from '../../assets/table_arrow.png';
+
+import Header from '../../components/Header';
+import BaseMenu from '../../components/BaseMenu';
+
+import {
+  Container,
+  Title,
+  H1,
+  H2,
+  Table,
+  TableText,
+  HeadItems,
+  HeadText,
+  CartItems,
+  CartItem,
+  CartItemText,
+  CartItemSubtext,
+  OrderButton,
+  OrderButtonText,
+  CancelOrderText,
+} from './styles';
 
 const Order: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <Header />
+      <ScrollView style={{ flex: 1 }}>
+        <Container>
+          <Title>
+            <Image source={backArrow} style={{ marginRight: 15 }} />
+            <H1>Seu pedido</H1>
+          </Title>
+          <H2>Local</H2>
+          <Table>
+            <TableText>Mesa 6</TableText>
+            <Image source={tableArrow} />
+          </Table>
+          <H2>Detalhes dos pedidos</H2>
+          <HeadItems>
+            <HeadText>Item</HeadText>
+            <HeadText>Quantidade</HeadText>
+            <HeadText>Preço</HeadText>
+          </HeadItems>
+          <CartItems>
+            <CartItem>
+              <CartItemText>Budweiser</CartItemText>
+              <CartItemSubtext>10</CartItemSubtext>
+              <CartItemSubtext>R$ 8,90</CartItemSubtext>
+            </CartItem>
+          </CartItems>
+          <Table>
+            <CartItemSubtext>Total</CartItemSubtext>
+            <TableText>R$ 89,00</TableText>
+          </Table>
+          <OrderButton onPress={() => navigation.navigate('Splash')}>
+            <OrderButtonText>Pedir agora</OrderButtonText>
+          </OrderButton>
+          <CancelOrderText>Cancelar pedido</CancelOrderText>
+        </Container>
+      </ScrollView>
+      <BaseMenu />
+    </>
+  );
 };
 
 export default Order;
--- src/pages/QRCode/index.tsx
+++ src/pages/QRCode/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Image, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import BaseMenu from '../../components/BaseMenu';
 
 import QRCode from '../../assets/QRCode/QRCode.png';
 import CloseIcon from '../../assets/close.png';
@@ -12,29 +11,25 @@ import { Container, H1, P, CloseButton } from './styles';
 const Rewards: React.FC = () => {
   const navigation = useNavigation();
   return (
-    <>
-      <Container>
-        <CloseButton onPress={() => navigation.goBack()}>
-          <Image source={CloseIcon} />
-        </CloseButton>
-        <TouchableOpacity
-          style={{ alignItems: 'center' }}
-          onPress={() => navigation.navigate('Menu')}
-        >
-          <ScrollView
-            contentContainerStyle={{ alignItems: 'center', width: '100%' }}
-          />
-          <Image source={QRCode} />
-          <H1>QRCode do Bar</H1>
-          <P>
-            Abra sua câmera, escaneie o QRCode do Bar e tenha acesso ao
-            cardápio.
-          </P>
-          <ScrollView />
-        </TouchableOpacity>
-      </Container>
-      <BaseMenu />
-    </>
+    <Container>
+      <CloseButton onPress={() => navigation.goBack()}>
+        <Image source={CloseIcon} />
+      </CloseButton>
+      <TouchableOpacity
+        style={{ alignItems: 'center' }}
+        onPress={() => navigation.navigate('Menu')}
+      >
+        <ScrollView
+          contentContainerStyle={{ alignItems: 'center', width: '100%' }}
+        />
+        <Image source={QRCode} />
+        <H1>QRCode do Bar</H1>
+        <P>
+          Abra sua câmera, escaneie o QRCode do Bar e tenha acesso ao cardápio.
+        </P>
+        <ScrollView />
+      </TouchableOpacity>
+    </Container>
   );
 };
 
